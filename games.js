4c153e03a5712b3ce2863e8e7c7fb2c99772f8ac
awnetcode <155958835+awnetcode@users.noreply.github.com>
2024-11-29 19:25:46 +0100

    snake-grow

--- games.js
+++ games.js
@@ -7,8 +7,9 @@ const gridHeight = 20;
 
 let snakeDirection = 'up';
 let gameInterval;
-let gameSpeed = 500;
+let gameSpeed = 200;
 let gameStarted = false;
+
 
 let snake = [{x: 15, y: 10}];
 
@@ -16,6 +17,7 @@ let snake = [{x: 15, y: 10}];
 const drawGame = () => {
     boardElement.innerHTML = '';
     drawSnake();
+    drawFood();
 };
 
 const drawSnake = () => {
@@ -25,6 +27,22 @@ const drawSnake = () => {
         boardElement.appendChild(snakeFragment);
     });
 };
+
+const drawFood = () =>{
+    createFood();
+    const food = createElement("div", "food");
+    setElementPosition(food, foodPosition);
+    boardElement.appendChild(food);
+}
+
+const createFood = () =>{
+    const x = Math.floor(Math.random() * gridWidth) +1;
+    const y = Math.floor(Math.random() * gridHeight) +1;
+    return {x,y};
+}
+
+let foodPosition = createFood();
+console.log(foodPosition);
 
 //creates elements of snake or food
 const createElement = (tag, className) => {
@@ -41,52 +59,77 @@ const setElementPosition = (element, position) => {
 // Moves the snake based on direction
 const moveSnake = () => {
     const snakeHead = { ...snake[0] }; // Kopiowanie aktualnej głowy węża
-    switch (snakeDirection) {
-        case 'ArrowUp':
-            snakeHead.y--;
-            break;
-        case 'ArrowDown':
-            snakeHead.y++;
-            break;
-        case 'ArrowLeft':
-            snakeHead.x--;
-            break;
-        case 'ArrowRight':
-            snakeHead.x++;
-            break;
+    if(gameStarted == true){
+        switch (snakeDirection) {
+            case 'ArrowUp':
+                snakeHead.y--;
+                break;
+            case 'ArrowDown':
+                snakeHead.y++;
+                break;
+            case 'ArrowLeft':
+                snakeHead.x--;
+                break;
+            case 'ArrowRight':
+                snakeHead.x++;
+                break;
+        }
     }
     snake.unshift(snakeHead); // Dodaj nową głowę
-    snake.pop(); // Usuń ostatni segment
+    if(snakeHead.x === foodPosition.x && snakeHead.y === foodPosition.y){
+        foodPosition = createFood();
+        clearInterval(gameInterval);
+        gameInterval = setInterval(()=>{
+            moveSnake();
+            drawGame();
+        }, gameSpeed);
+    }else{
+snake.pop();
+    }
+     // Usuń ostatni segment
 };
 
 // Obsługuje naciśnięcia klawiszy
  document.addEventListener('keydown', (event) => {
-     switch (event.key) {
-         case 'ArrowUp':
-             if (snakeDirection !== 'ArrowDown') snakeDirection = 'ArrowUp';
-             break;
-         case 'ArrowDown':
-             if (snakeDirection !== 'ArrowUp') snakeDirection = 'ArrowDown';
-             break;
-         case 'ArrowLeft':
-             if (snakeDirection !== 'ArrowRight') snakeDirection = 'ArrowLeft';
-             break;
-         case 'ArrowRight':
-             if (snakeDirection !== 'ArrowLeft') snakeDirection = 'ArrowRight';
-             break;
-     }
+        switch (event.key) {
+            case 'ArrowUp':
+                if (snakeDirection !== 'ArrowDown') snakeDirection = 'ArrowUp';
+                break;
+            case 'ArrowDown':
+                if (snakeDirection !== 'ArrowUp') snakeDirection = 'ArrowDown';
+                break;
+            case 'ArrowLeft':
+                if (snakeDirection !== 'ArrowRight') snakeDirection = 'ArrowLeft';
+                break;
+            case 'ArrowRight':
+                if (snakeDirection !== 'ArrowLeft') snakeDirection = 'ArrowRight';
+                break;
+        }
  });
 
 // Startuje grę
 const startGame = () => {
-    if (!gameStarted) {
-        gameStarted = true;
-        gameInterval = setInterval(() => {
-            moveSnake();
-            drawGame();
-        }, gameSpeed);
-    }
+    gameInterval = setInterval(() => {
+        moveSnake();
+        drawFood();
+        drawGame();
+    }, gameSpeed);
+    gameStarted = true;
+};
+
+const pauseGame = () =>{
+    document.addEventListener("keydown", (event) => {
+        if (event.key === ' ') {
+            if (gameStarted) {
+                clearInterval(gameInterval); // Zatrzymanie gry
+                gameStarted = false;
+            } else {
+                startGame(); // Rozpoczęcie gry
+            }
+        }
+    });
 };
 
 drawGame();
 startGame();
+pauseGame();
